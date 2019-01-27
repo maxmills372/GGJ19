74cbb50b1f6c7a0b16b9ac4b0b1c5e74a1a424d3
maxmills372 <topmanmax@gmail.com>
2019-01-27 01:01:54 +0000

    added main menu and particlez

--- HomewardRound/Assets/Scripts/BouncingPlayer.cs
+++ HomewardRound/Assets/Scripts/BouncingPlayer.cs
@@ -2,15 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent (typeof(Rigidbody))]
 public class BouncingPlayer : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+    public float force = 10.0f;
+    Rigidbody rb;
+    bool grounded = false;
+
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        float horiz = Input.GetAxis("Horizontal");
+        float vert = Input.GetAxis("Vertical");
+        if(Input.GetKey(KeyCode.Space) & grounded)
+        {
+            rb.AddForce(Vector3.up * force * 10.0f);
+            grounded = false;
+        }
+        rb.AddForce(new Vector3(horiz * force, 0.0f, vert * force));
+
+    }
+
+    
 }
