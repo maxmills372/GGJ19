30ec3abea4af1c670dc575513e3e482597e26034
maxmills372 <topmanmax@gmail.com>
2019-01-27 12:09:20 +0000

    made man manu work

--- HomewardRound/Assets/Scripts/ChangeBallLayer.cs
+++ HomewardRound/Assets/Scripts/ChangeBallLayer.cs
@@ -1,24 +1,52 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class ChangeBallLayer : MonoBehaviour {
 
     public int LayerOnEnter; // BallInHole
     public int LayerOnExit;  // BallOnTable
 	
+    public enum TriggerType { Start,Exit,Options};
+    public TriggerType trigger_type; 
+
     void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.tag == "Player")
         {
             other.gameObject.layer = LayerOnEnter;
+            StartCoroutine("StartGame");
         }
     }
 
     void OnTriggerExit(Collider other)
     {
-        if(other.gameObject.tag == "Player")
+        if (other.gameObject.tag == "Player")
         {
             other.gameObject.layer = LayerOnExit;
         }
+
+    }
+    IEnumerator StartGame()
+    {
+        yield return new WaitForSeconds(2.0f);
+        switch(trigger_type)
+        {
+            case TriggerType.Start:
+                SceneManager.LoadSceneAsync(1, LoadSceneMode.Single);
+
+                break;
+
+            case TriggerType.Options:
+                SceneManager.LoadSceneAsync(0, LoadSceneMode.Single);
+
+                break;
+            case TriggerType.Exit:
+                Application.Quit();
+                break;
+            default: break;
+        }
+
+        yield return null;
     }
 }
